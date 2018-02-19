db4b46b9db644428e89ad5250bf92b2e98dddfa3
Jhonatan Plata <kaysinhoadsi@hotmail.com>
2018-02-19 15:35:10 -0500

    ajuste final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCxxtRPmcpwD6UEJi31qkYql5Qyh5I3aGs",
+    authDomain: "mistareas-b85b8.firebaseapp.com",
+    databaseURL: "https://mistareas-b85b8.firebaseio.com",
+    projectId: "mistareas-b85b8",
+    storageBucket: "mistareas-b85b8.appspot.com",
+    messagingSenderId: "21269038795"
+  }
 };
